920b7738cfa8e009f076e3600dfbacf8389da21f
Andriy Semenistiy <46621668+MeowningMaster@users.noreply.github.com>
2021-11-02 01:11:21 +0200

    aho corasik

---
+++ src/aho_corasik.hpp
@@ -0,0 +1,92 @@
+#pragma once
+#include <array>
+#include <vector>
+#include <string>
+using namespace std;
+
+const size_t k = 26;
+struct vertex {
+    array<vertex*, k> to = {0}, go = {0};
+    vertex* link = 0;
+    vertex* p;
+    size_t pch, len;
+    bool leaf = false;
+
+    vertex(size_t _pch, vertex* _p, size_t _len): pch(_pch), p(_p), len(_len) {}
+};
+
+struct dictionary_automaton {
+    vertex* root;
+
+    void init() {
+        root = new vertex(-1, 0, 0);
+    }
+
+    dictionary_automaton() {
+        init();
+    }
+
+    void fill(vector<string>& dictionary) {
+        for (string word : dictionary) {
+            add_word(word);
+        }
+    }
+
+    dictionary_automaton(vector<string>& dictionary) {
+        init();
+        fill(dictionary);
+    }
+    
+    void add_word(string& word) {
+        vertex* v = root;
+        for (char ch : word) {
+            size_t c = ch - 'a';
+            if (!v->to[c]) {
+                v->to[c] = new vertex(c, v, v->len + 1);
+            }  
+            v = v->to[c];
+        }
+        v->leaf = true;
+    }
+
+    vertex* link(vertex* v) {
+        if (!v->link) {
+            if (v == root || v->p == root) {
+                v->link = root;
+            } else {
+                v->link = go(link(v->p), v->pch);
+            }
+        }
+        return v->link;
+    }
+
+    vertex* go(vertex* v, char ch) {
+        if (ch <= 'a' || ch >= 'z') {
+            return root;
+        }
+
+        size_t c = ch - 'a';
+        if (!v->go[c]) {
+            if (v->to[c]) {
+                v->go[c] = v->to[c];
+            } else if (v == root) {
+                v->go[c] = root;
+            } else {
+                v->go[c] = go(link(v), c);
+            }     
+        }
+        return v->go[c];
+    }
+};
+
+vector<size_t> aho_corasik_search(const string& text, dictionary_automaton dictionary) {
+    vector<size_t> entries;
+    vertex* v = dictionary.root;
+    for (size_t i = 0; i < text.length(); i++) {
+        v = dictionary.go(v, text[i]);
+        if (v->leaf) {
+            entries.push_back(i - v->len);
+        }
+    }
+    return entries;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,13 @@
 #include "join.hpp"
 #include "timer.hpp"
 #include "dumb_search.hpp"
+#include "aho_corasik.hpp"
 using namespace std;
 
 int main() {
     string text = join(read_file("../assets/text.txt"), '\n');
     vector<string> dictionary = read_file("../assets/dictionary.txt");
 
-    cout << measure_nanoseconds([&](){ dumb_search(text, dictionary); });
+    cout << measure_nanoseconds([&](){ dumb_search(text, dictionary); }) << '\n';
+    cout << measure_nanoseconds([&](){ aho_corasik_search(text, dictionary_automaton(dictionary)); }) << '\n';
 }
